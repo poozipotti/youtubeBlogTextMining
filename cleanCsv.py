e344305d7584593b49e5e20ec92f77380b41ad08
George Beckert <georgebeckert@gmail.com>
2018-08-09 14:13:16 -0400

    initial commit

--- cleanCsv.py
+++ cleanCsv.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-pd.read_csv('foo.csv')
+
+pd.Series([1,2,3,4,5])
+`
+print "hello"
+
+
--- cleaner.py
+++ cleaner.py
@@ -5,17 +5,18 @@ pattern = "*.*"
 #we dont need this yet but python is fun
 #print theDir
 def scrubTags(dirtyString):
-	tag = "<.*>"	
-	tagRegex = re.compile(tag)
-	cleanString = tagRegex.sub("",dirtyString)
-	cleanString = cleanString.replace("\n","")
+	cleanString = re.sub("<.*>","",dirtyString)
 	cleanString = cleanString.translate(None,string.punctuation)
+	cleanString = re.sub("\s\s+"," ", cleanString)
+	cleanString = cleanString.replace("\n","")
 	return cleanString
 
 #this will look for a match in the line so any unique identifier will so
 sectionsToSave = ["class=\'post-content\'","class=\'publishdate\'"]	
 
 #filenemaes should be relative to wherever this script is
+#we know that the date is in a span so after we get a span write a comma
+#this is very brittle so it may have to be changed later
 def cleanStandardBlogPost(filename):
 	readFile = open(filename,"r")
 	writeFile = open("CLEAN_BLOG.csv","a+")
@@ -45,7 +46,7 @@ def addAllFiles(folder):
 		theDir = os.listdir(folder)
 		files = (list(map( lambda fileName: folder + "/" + fileName,theDir)))
 		directories = (list(filter(lambda directoryEntry: ".html" not in directoryEntry,files)))
-		files = (list(filter(lambda directoryEntry: ".html" in directoryEntry,files)))
+		files = (list(filter(lambda directoryEntry: (".html" in directoryEntry) and ("?" not in directoryEntry) and ("index" not in directoryEntry) , files)))
 		#print files
 		for htmlFile in files:
 			cleanStandardBlogPost(htmlFile)
@@ -54,4 +55,8 @@ def addAllFiles(folder):
 			addAllFiles(directory) 
 	except Exception as e:
 		print e
+
+#this is the stuff that actually runs once when the file is called from the command line
+if os.path.exists("CLEAN_BLOG.csv"):
+	os.remove("CLEAN_BLOG.csv")
 addAllFiles(sys.argv[1])
